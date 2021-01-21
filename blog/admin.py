01988ad97859ad853765908e68fdd4a4e6240f53
Jakub Pacanowski <jakub.pacanowski@gmail.com>
2021-01-21 21:49:37 +0100

    Admin site: add Post model fields

--- blog/admin.py
+++ blog/admin.py
@@ -5,4 +5,10 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+
     list_display = ('title', 'author', 'published_at', 'status')
+    list_filter = ('status', 'created_at', 'published_at', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    ordering = ('status', '-published_at')

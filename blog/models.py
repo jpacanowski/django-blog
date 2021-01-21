86df6ffc269430e76d69d3bbb50150f75af7d576
Jakub Pacanowski <jakub.pacanowski@gmail.com>
2021-01-21 10:18:44 +0100

    Create Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique_for_date='published_at')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    body = models.TextField()
+    published_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    class Meta:
+        ordering = ('-published_at',)
+
+    def __str__(self):
+        return self.title

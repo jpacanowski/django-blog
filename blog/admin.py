24fc88da66ff80fafa751fabe21d6cbe638f3052
Jakub Pacanowski <jakub.pacanowski@gmail.com>
2021-01-22 09:46:14 +0100

    Admin site: remove raw_id_fields

--- blog/admin.py
+++ blog/admin.py
@@ -10,5 +10,4 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('status', 'created_at', 'published_at', 'author')
     search_fields = ('title', 'body')
     prepopulated_fields = {'slug': ('title',)}
-    raw_id_fields = ('author',)
     ordering = ('status', '-published_at')

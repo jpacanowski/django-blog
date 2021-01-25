c89ff93681142f458ed83963b0000d9a37798ebc
Jakub Pacanowski <jakub.pacanowski@gmail.com>
2021-01-25 14:53:09 +0100

    Add unique flag for slug field

--- blog/models.py
+++ blog/models.py
@@ -19,7 +19,7 @@ class Post(models.Model):
     )
 
     title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, unique_for_date='published_at')
+    slug = models.SlugField(max_length=255, unique=True, null=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     published_at = models.DateTimeField(default=timezone.now)

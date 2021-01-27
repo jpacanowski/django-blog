1fefa337016fce291633109c8fb893f92b8f96bc
Jakub Pacanowski <jakub.pacanowski@gmail.com>
2021-01-27 15:33:36 +0100

    Add Comment model

--- blog/models.py
+++ blog/models.py
@@ -38,3 +38,18 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', args=[self.slug])
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return 'Comment created by {} for the post {}'.format(self.name, self.post)

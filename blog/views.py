96482dd84b9b361cf30cb9a5dafb02b17dcfca7f
Jakub Pacanowski <jakub.pacanowski@gmail.com>
2021-01-29 10:09:18 +0100

    Add comments system

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
-from .models import Post
+from .forms import CommentForm
+from .models import Post, Comment
 
 # Create your views here.
 
@@ -33,5 +34,23 @@ def post_list(request):
         'posts': posts})
 
 def post_detail(request, post):
+
     post = get_object_or_404(Post, slug=post, status='published')
-    return render(request, 'blog/post/single.html', {'post': post})
+    comments = post.comments.filter(active=True)
+
+    if request.method == 'POST':
+        # Komentarz został opublikowany
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            # Utworzenie obiektu Comment, ale jeszcze nie zapisujemy go w bazie danych
+            new_comment = comment_form.save(commit=False)
+            # Przypisanie komentarza do bieżącego posta
+            new_comment.post = post
+            # Zapisanie komentarza w bazie danych
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'blog/post/single.html',
+        {'post': post, 'comments': comments, 'comment_form': comment_form})

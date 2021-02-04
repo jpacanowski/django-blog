0880879cd0ba2af4e611bf9fb26b3209ee9aacc9
Jakub Pacanowski <jakub.pacanowski@gmail.com>
2021-02-04 11:32:54 +0100

    Create show_most_commented_posts template tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django import template
 from ..models import Post
 
@@ -6,6 +7,13 @@ register = template.Library()
 @register.simple_tag
 def total_posts():
     return Post.published.count()
+
+@register.inclusion_tag('blog/post/most_commented_posts.html')
+def show_most_commented_posts(count=5):
+    most_commented_posts = Post.published.annotate(
+        total_comments=Count('comments')
+        ).order_by('-total_comments')[:count]
+    return {'most_commented_posts': most_commented_posts}
 
 @register.inclusion_tag('blog/post/latest_posts.html')
 def show_latest_posts(count=5):

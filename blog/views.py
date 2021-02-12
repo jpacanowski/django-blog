9bfeb343c72c69f27bc41c26fe75c6e6851fb497
Jakub Pacanowski <jakub.pacanowski@gmail.com>
2021-02-12 10:11:34 +0100

    Post searching: Add SearchRank

--- blog/views.py
+++ blog/views.py
@@ -117,12 +117,18 @@ def post_search(request):
     results = []
 
     if 'query' in request.GET:
+
         form = SearchForm(request.GET)
+
         if form.is_valid():
             query = form.cleaned_data['query']
+
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = Post.objects.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
 
     return render(request, 'blog/post/search.html', {
         'form': form,
